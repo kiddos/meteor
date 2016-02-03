c6253dda8e32befca8555aff1dac3c8bf40ea179
kiddos <josephyu831007@yahoo.com.tw>
2016-02-03 08:20:41 +0800

    function to check collision with meteor

--- src/bullet.h
+++ src/bullet.h
@@ -2,6 +2,7 @@
 #define BULLET_H
 
 #include "common.h"
+#include "meteor.h"
 
 /* constants */
 extern const double BULLET_SPEED;
@@ -19,6 +20,8 @@ bullet *bullet_init(const point start, const double direction);
 void bullet_add(bullet *bullets, const point start, const double direction);
 size_t bullet_get_count(bullet * const bullets);
 bool bullet_is_inside_screen(bullet *b, const size window_size);
+bool bullet_hit_meteor(bullet *b, const meteor *m);
+bool bullet_check_collision(bullet *bullets, const meteor *m);
 void bullet_update(bullet *bullets, const size window_size);
 void bullet_draw(bullet *bullets);
 void bullet_destroy(bullet *bullets);

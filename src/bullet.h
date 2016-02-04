b50856a29bb456397bd19d138d6f21203f19958f
kiddos <josephyu831007@yahoo.com.tw>
2016-02-04 10:25:01 +0800

    bullet usage and clean up bullet in update function

--- src/bullet.h
+++ src/bullet.h
@@ -13,15 +13,20 @@ typedef struct bullet_t {
   point start;
   velocity v;
   double speed, direction;
+  bool is_used;
   struct bullet_t *next;
 } bullet;
 
-bullet *bullet_init(const point start, const double direction);
-void bullet_add(bullet *bullets, const point start, const double direction);
+bullet *bullet_init(const point start, const double direction,
+                    const double damage);
+void bullet_add(bullet *bullets, const point start,
+                const double direction, const double damage);
 size_t bullet_get_count(bullet * const bullets);
 bool bullet_is_inside_screen(bullet *b, const size window_size);
 bool bullet_hit_meteor(bullet *b, const meteor *m);
-bool bullet_check_collision(bullet *bullets, const meteor *m);
+bool bullet_check_collision(bullet *bullets, meteor *m);
+bool bullet_is_used(bullet *b);
+void bullet_use(bullet *b);
 void bullet_update(bullet *bullets, const size window_size);
 void bullet_draw(bullet *bullets);
 void bullet_destroy(bullet *bullets);

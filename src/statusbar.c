75959539bdfd3d723643369fdbf80821a5ef187e
kiddos <josephyu831007@yahoo.com.tw>
2016-02-08 22:12:33 +0800

    rename regrouped module

--- src/statusbar.c
+++ src/statusbar.c
@@ -48,9 +48,11 @@ status_bar *status_bar_init(ship *s, meteor_shower *ms,
       free(sb);
       return NULL;
     }
-    sb->td = time_display_init(display_font, color_white(),
-                               point_init(sb->start.x + STATUS_BAR_PADDING,
-                                          sb->start.y + STATUS_BAR_PADDING));
+    sb->display.td = time_display_init(display_font, color_white(),
+                                       point_init(sb->start.x +
+                                                  STATUS_BAR_PADDING,
+                                                  sb->start.y +
+                                                  STATUS_BAR_PADDING));
 
     // score
     display_font = al_load_font(FONT_PATH,
@@ -60,14 +62,15 @@ status_bar *status_bar_init(ship *s, meteor_shower *ms,
       error_message("fail to create font for score");
       al_destroy_font(sb->font);
       al_destroy_bitmap(sb->live_icon);
-      time_display_destroy(sb->td);
+      time_display_destroy(sb->display.td);
       free(sb);
       return NULL;
     }
-    sb->sco = score_init(display_font, color_white(),
-                         point_init(time_display_get_text_width(sb->td) +
-                                    6 * STATUS_BAR_PADDING,
-                                    sb->start.y + STATUS_BAR_PADDING));
+    sb->display.s = score_init(display_font, color_white(),
+                               point_init(time_display_get_text_width(
+                                          sb->display.td) +
+                                          6 * STATUS_BAR_PADDING,
+                                          sb->start.y + STATUS_BAR_PADDING));
 
     sb->object.s = s;
     sb->object.ms = ms;
@@ -79,31 +82,39 @@ status_bar *status_bar_init(ship *s, meteor_shower *ms,
 }
 
 void status_bar_start(status_bar *sb) {
-  if (sb != NULL && sb->td != NULL) {
-    time_display_start(sb->td);
+  if (sb != NULL && sb->display.td != NULL) {
+    time_display_start(sb->display.td);
   } else {
     error_message("unable to start status bar | null pointer");
   }
 }
 
 void status_bar_reset(status_bar *sb) {
-  if (sb != NULL && sb->td != NULL) {
-    time_display_stop(sb->td);
-    score_reset(sb->sco);
+  if (sb != NULL && sb->display.td != NULL) {
+    time_display_stop(sb->display.td);
+    score_reset(sb->display.s);
   } else {
     error_message("unable to reset status bar | null pointer");
+  }
+}
+
+uint64_t status_bar_get_score(const status_bar *sb) {
+  if (sb != NULL) {
+    return score_get_total_score(sb->display.s);
   }
 }
 
 void status_bar_update(status_bar *sb, const size window_size) {
   if (window_size.h - STATUS_BAR_HEIGHT != sb->start.y) {
     sb->start.y = window_size.h - STATUS_BAR_HEIGHT;
-    time_display_set_point(sb->td, point_init(STATUS_BAR_PADDING,
-                                              sb->start.y + STATUS_BAR_PADDING));
+    time_display_set_point(sb->display.td, point_init(STATUS_BAR_PADDING,
+                                                      sb->start.y +
+                                                      STATUS_BAR_PADDING));
 
-    score_set_point(sb->sco, point_init(time_display_get_text_width(sb->td) +
-                                        6 * STATUS_BAR_PADDING,
-                                        sb->start.y + STATUS_BAR_PADDING));
+    score_set_point(sb->display.s,
+                    point_init(time_display_get_text_width(sb->display.td) +
+                               6 * STATUS_BAR_PADDING,
+                               sb->start.y + STATUS_BAR_PADDING));
   }
 
   sb->start.x = 0;
@@ -111,24 +122,25 @@ void status_bar_update(status_bar *sb, const size window_size) {
   sb->area.h = STATUS_BAR_HEIGHT;
 
   // add score
-  score_set_time_score(sb->sco, time_display_get_time_passed(sb->td));
+  score_set_time_score(sb->display.s,
+                       time_display_get_time_passed(sb->display.td));
   meteor_count count = meteor_shower_get_destroy_count(sb->object.ms);
   uint32_t i;
   for (i = 0 ; i < count.small ; i ++) {
-    score_add_score_type(sb->sco, SCORE_METEOR_SMALL);
+    score_add_score_type(sb->display.s, SCORE_METEOR_SMALL);
     regular_message("******** score small added *******");
   }
   for (i = 0 ; i < count.medium ; i ++) {
-    score_add_score_type(sb->sco, SCORE_METEOR_MEDIUM);
+    score_add_score_type(sb->display.s, SCORE_METEOR_MEDIUM);
     regular_message("******** score medium added *******");
   }
   for (i = 0 ; i < count.large ; i ++) {
-    score_add_score_type(sb->sco, SCORE_METEOR_LARGE);
+    score_add_score_type(sb->display.s, SCORE_METEOR_LARGE);
     regular_message("******** score large added *******");
   }
 
-  score_update(sb->sco);
-  time_display_update(sb->td);
+  score_update(sb->display.s);
+  time_display_update(sb->display.td);
 }
 
 void status_bar_draw(status_bar *sb) {
@@ -149,8 +161,8 @@ void status_bar_draw(status_bar *sb) {
                     sb->start.y + sb->area.h,
                     sb->background_color);
 
-  time_display_draw(sb->td);
-  score_draw(sb->sco);
+  time_display_draw(sb->display.td);
+  score_draw(sb->display.s);
 
   for (i = 0 ; i < ship_get_lives(sb->object.s) ; i ++) {
     al_draw_scaled_bitmap(sb->live_icon, 0, 0,
@@ -170,14 +182,14 @@ void status_bar_draw(status_bar *sb) {
 
 void status_bar_destroy(status_bar *sb) {
   if (sb != NULL) {
+    if (sb->display.td)
+      time_display_destroy(sb->display.td);
+    if (sb->display.s)
+      score_destroy(sb->display.s);
     if (sb->font)
       al_destroy_font(sb->font);
     if (sb->live_icon)
       al_destroy_bitmap(sb->live_icon);
-    if (sb->td)
-      time_display_destroy(sb->td);
-    if (sb->sco)
-      score_destroy(sb->sco);
 
     free(sb);
     regular_message("destroy status bar");

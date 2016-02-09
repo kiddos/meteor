bd79f5ba541d1597d9ecb1de044411965c4b02a7
kiddos <josephyu831007@yahoo.com.tw>
2016-02-09 22:39:36 +0800

    constants for drawing and declare update function

--- src/inputdialog.h
+++ src/inputdialog.h
@@ -5,18 +5,29 @@
 
 #define INPUT_DIALOG_BUFFER_SIZE 1024
 
+extern const double INPUT_DIALOG_MIN_WIDTH;
+extern const double INPUT_DIALOG_MIN_HEIGHT;
+extern const double INPUT_DIALOG_PROMPT_FONT_SIZE;
+extern const double INPUT_DIALOG_PADDING;
+
 typedef struct input_dialog_t {
+  ALLEGRO_FONT *font;
   ALLEGRO_COLOR border_color;
   char *prompt;
   char text[INPUT_DIALOG_BUFFER_SIZE];
   bool should_intercept_keyboard_input;
+  point center;
+  size display_size;
 } input_dialog;
 
 input_dialog *input_dialog_init(const char * const prompt,
-                                const ALLEGRO_COLOR border_color);
+                                const ALLEGRO_COLOR border_color,
+                                const char * const font_path,
+                                const size window_size);
 void input_dialog_retrieve_text(const input_dialog *id,
                                 char text[INPUT_DIALOG_BUFFER_SIZE]);
 void input_dialog_enter_char(input_dialog *id, const char c);
+void input_dialog_update(input_dialog *id, const size window_size);
 void input_dialog_draw(const input_dialog *id);
 void input_dialog_destroy(input_dialog *id);
 
